0e2a96f3a7e914cbfe0fdafbe01f460a4f431cfb
Gabriel Pinto <gpinto.personal@gmail.com>
2026-02-24 23:28:41 -0400

    feat: add welcome component with image display and layout integration

--- public/build/assets/IndexDocument-50820993.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{b as c,d as o}from"./app-7e2791a7.js";const r={};function t(n,s){return c(),o("div")}const f=e(r,[["render",t]]);export{f as default};
