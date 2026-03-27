88544d62b16968d0cf9c0c3c01228c0427c9e8aa
Gabriel Pinto <gpinto.personal@gmail.com>
2026-03-26 23:47:33 -0400

    feat: add welcome page component with image display

---
+++ public/build/assets/EditDocument-586bd41c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{b as t,d as c}from"./app-3c8ad215.js";const o={};function r(n,s){return t(),c("div")}const f=e(o,[["render",r]]);export{f as default};
---
+++ public/build/assets/IndexDocument-262afa94.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{b as c,d as o}from"./app-3c8ad215.js";const r={};function t(n,s){return c(),o("div")}const f=e(r,[["render",t]]);export{f as default};
