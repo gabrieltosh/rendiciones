ff12310fcad90a4444d68b3f6e9953cc6314a67e
Gabriel Pinto <gpinto.personal@gmail.com>
2026-04-18 15:31:42 -0400

    Arreglo de migraciones

--- database/migrations/2026_04_17_232421_create_account_aliases_table.php
+++ database/migrations/2026_04_17_232421_create_account_aliases_table.php
@@ -11,6 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('account_aliases')) {
+            return;
+        }
         Schema::create('account_aliases', function (Blueprint $table) {
             $table->id();
             $table->string('acct_code')->unique();
